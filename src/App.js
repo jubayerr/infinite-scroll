0eda0062bf15088e6d073ea91d746ffea2ba5c09
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-04-22 20:20:33 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 import useBookSearch from './useBookSearch';
 
 function App() {
+
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
-  useBookSearch(query, pageNumber)
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
   return (
     <div className="App">
-      <input type="text" />
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>...loading</div>
-      <div>error!</div>
+      <input type="text" onChange={handleSearch} />
+      {books.map(book => {
+        return <div key={book}>{book}</div>
+      })}
+      <div>{loading && "...loading"}</div>
+      <div>{error && "Error!"}</div>
     </div >
   );
 }
